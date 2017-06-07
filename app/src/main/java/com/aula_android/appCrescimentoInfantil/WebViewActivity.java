2dbb710d7727e526cfc24a85dc15b42daa164dc3
allan.lemes <aflemess@gmail.com>
2017-06-07 13:08:51 -0300

    .

--- app/src/main/java/com/aula_android/appCrescimentoInfantil/WebViewActivity.java
+++ app/src/main/java/com/aula_android/appCrescimentoInfantil/WebViewActivity.java
@@ -28,16 +28,35 @@ public class WebViewActivity extends AppCompatActivity {
         registros =(ArrayList<ArrayList<String>>) extras.getSerializable(ViewCriancaActivity.REGISTROS);
 
 
-        /*webView = (WebView) findViewById(R.id.webViewCrescimento);
+        webView = (WebView) findViewById(R.id.webViewCrescimento);
         webView.getSettings().setJavaScriptEnabled(true);
+        /*
         webView.loadUrl("https://chart.googleapis.com/chart?cht=lc&chco=FF6342,ADDE63,63C6DE&chs=250x100&chd=t:6.92,8.98,10.5,11.6%7C7.5,10.12,12.3,15.25%7C8.76,11.25,13.02,14.33&chl=6%20meses%7C1%20ano%7C1a%206m%7C2%20anos&chdl=Peso%20Min.%7CPeso%20M%C3%A1x.%7C%20Peso");
         */
 
         ArrayList<String> tempNodo;
         for (int i = 0; i < registros.size();i ++){
             tempNodo = registros.get(i);
-
         }
+
+        String googleURL    = "https://chart.googleapis.com/chart";
+        String tipoGrafico  = "cht=lc"; //linhas
+        String coresGrafico = "&chco=FF6342,ADDE63,63C6DE";
+        String dimenGrafico = "&chs=250x100";
+        String dadosGrafico = "&chd=t:6.92,8.98,10.5,11.6"   + "|" +
+                                      "7.5,10.12,12.3,15.25" + "|" +
+                                      "8.76,11.25,13.02,14.33";
+        String titEixoX     = "&chl=6%20meses|1%20ano|1a%206m|2%20anos";
+        String legGrafico   = "&chdl=Peso%20Min.|Peso%20M%C3%A1x.|%20Peso";
+
+        webView.loadUrl(googleURL +
+                        tipoGrafico +
+                        coresGrafico +
+                        dimenGrafico +
+                        dadosGrafico +
+                        titEixoX +
+                        legGrafico);
+
     }
 
 }
